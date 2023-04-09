442e75952d3b574011c539370d779a36f553645b
Henry Van Rooyen <hendrik.vanrooyen@adesso.de>
2023-04-09 09:23:19 +0200

    ngrx: install effects library

--- frontends/ngrx/src/app/app.module.ts
+++ frontends/ngrx/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { StoreModule } from '@ngrx/store';
 import { usersReducer } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { usersReducer } from './reducers';
     MatSelectModule,
     FormsModule,
     MatButtonModule,
-    StoreModule.forRoot(usersReducer)
+    StoreModule.forRoot(usersReducer),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
