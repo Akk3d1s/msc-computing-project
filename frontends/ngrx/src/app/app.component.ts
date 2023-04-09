0276e7e363fca16663876208f15f586adab52940
Henry Van Rooyen <hendrik.vanrooyen@adesso.de>
2023-04-09 10:41:26 +0200

    ngrx: update frontend to display data

--- frontends/ngrx/src/app/app.component.ts
+++ frontends/ngrx/src/app/app.component.ts
@@ -1,69 +1,54 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { SelectionModel } from '@angular/cdk/collections';
 import { Store } from '@ngrx/store';
 import * as UserActions from "./state/actions";
 import * as UserSelectors from "./state/selectors";
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-];
+import { User } from 'src/app/interfaces/user.interface';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   title = 'Editing data using NGRX';
-  displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  selection = new SelectionModel<PeriodicElement>(true, []);
+  displayedColumns: string[] = ['select', 'userId', 'status', 'username', 'email', 'name', 'surname', 'birthdate', 'registeredAt'];
+  dataSource = new MatTableDataSource<User>([]);
+  selection = new SelectionModel<User>(true, []);
   statuses: string[] = ['ACTIVE', 'PENDING', 'INACTIVE'];
   statusSelected: string = '';
   resourceSelected: string = '10';
   actionsEnabled: boolean;
   dataSourceSelection: string[] = ['10', '100', '1000', '10000', '100000'];
+  private _unsubscribe$: Subject<void> = new Subject<void>();
 
+  constructor(private readonly store: Store) {
+  }
 
-  users$: any;
-
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-
-  constructor( private readonly store: Store){
+  ngOnInit(): void {
+    // @todo - add ability to pass resourceSelected as param to getUsers()
     this.store.dispatch(UserActions.getUsers());
-    this.users$ = this.store.select(UserSelectors.getUsers);
+
+    this.store.select(UserSelectors.getUsers)
+      .pipe(takeUntil(this._unsubscribe$))
+      .subscribe((users: User[]) => {
+        this.dataSource.data = users;
+      })
   }
 
   ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
+  }
+
+  ngOnDestroy(): void {
+    this._unsubscribe$.next();
+    this._unsubscribe$.complete();
   }
 
   /** Whether the number of selected elements matches the total number of rows. */
@@ -90,14 +75,6 @@ export class AppComponent implements AfterViewInit {
     if (!this.actionsEnabled) {
       this.statusSelected = ''; // restore status selection to default.
     }
-  }
-
-  /** The label for the checkbox on the passed row */
-  checkboxLabel(row?: PeriodicElement): string {
-    if (!row) {
-      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
-    }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
   }
 
   handleFetchingResource(): void {
