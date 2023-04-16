80da23b3b64f5aef2b4c0eb2a9a84e7d42846271
Henry Van Rooyen <hendrik.vanrooyen@adesso.de>
2023-04-16 13:29:39 +0200

    update test config

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -3,13 +3,14 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     "supportFile": false,
-    "video": true,
+    "video": false,
     defaultCommandTimeout: 100000,
     setupNodeEvents(on, config) {
       on('before:browser:launch', (browser = {}, launchOptions) => {
 
         if (browser.family === 'chromium' && browser.name !== 'electron') {
           // auto open devtools
+          // below args seem to do nothing :-(
           launchOptions.args.push('--auto-open-devtools-for-tabs')
           // launchOptions.args.push('--throttle-cpu-rate=6')
           launchOptions.args.push('--force-devtools-available')
