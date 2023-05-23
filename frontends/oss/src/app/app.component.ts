1960705c8b26ce8af8f0e63e0dee3e5a04a9f4d4
Henry Van Rooyen <hendrik.vanrooyen@adesso.de>
2023-05-23 15:06:46 +0200

    oss: cleanup

--- frontends/oss/src/app/app.component.ts
+++ frontends/oss/src/app/app.component.ts
@@ -7,37 +7,6 @@ import { Subject, takeUntil } from 'rxjs';
 import { User } from 'src/app/users/user.interface';
 import { UsersEndpoint } from 'src/app/users/users.endpoint';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-  status: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, status: 'ACTIVE', name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, status: 'ACTIVE', name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, status: 'PENDING', name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, status: 'INACTIVE', name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, status: 'ACTIVE', name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, status: 'ACTIVE', name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, status: 'ACTIVE', name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, status: 'ACTIVE', name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, status: 'ACTIVE', name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, status: 'ACTIVE', name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, status: 'ACTIVE', name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, status: 'ACTIVE', name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, status: 'ACTIVE', name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, status: 'ACTIVE', name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, status: 'ACTIVE', name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, status: 'ACTIVE', name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, status: 'ACTIVE', name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, status: 'ACTIVE', name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, status: 'ACTIVE', name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, status: 'ACTIVE', name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-];
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
